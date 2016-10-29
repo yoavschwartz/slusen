ddf5151ed6ff3c908eeb3033df82405176b13cbb
Yoav Schwartz <yoav.schwarz@gmail.com>
2016-10-29 12:58:48 +0200

    Initial commit

---
+++ Slusen/Slusen/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Slusen
+//
+//  Created by Yoav Schwartz on 29/10/16.
+//  Copyright © 2016 Slusen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
