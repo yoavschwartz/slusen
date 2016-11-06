6e563ba5ac165283ddb23c9080c78feb9ef60df4
Yoav Schwartz <yoav.schwarz@gmail.com>
2016-11-06 18:35:39 +0100

    Added items to order

--- Slusen/Slusen/Order.swift
+++ Slusen/Slusen/Order.swift
@@ -10,8 +10,31 @@ import Foundation
 
 struct Order {
     let id: Int
-    let number: Int
+    let number: Int?
+    let createdAt: Date?
+    let priceInCents: Int?
+    let products: [Product]
+    let fetchIdentifier: String?
+    let delieveredAt: Date?
     var status: OrderStatus
+
+    init(id: Int,
+        number: Int? = nil,
+        createdAt: Date? = nil,
+        priceInCents: Int? = nil,
+        products: [Product],
+        fetchIdentifier: String? = nil,
+        delieveredAt: Date? = nil,
+        status: OrderStatus) {
+        self.id = id
+        self.number = number
+        self.createdAt = createdAt
+        self.priceInCents = priceInCents
+        self.products = products
+        self.fetchIdentifier = fetchIdentifier
+        self.delieveredAt = delieveredAt
+        self.status = status
+    }
 }
 
 enum OrderStatus {
