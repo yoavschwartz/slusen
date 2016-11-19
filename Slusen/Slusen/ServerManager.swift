6c855640a4c1cd47d5033e3319151f83d3aead28
Yoav Schwartz <yoav.schwarz@gmail.com>
2016-11-19 20:00:13 +0100

    deleted comments

--- Slusen/Slusen/ServerManager.swift
+++ Slusen/Slusen/ServerManager.swift
@@ -34,13 +34,6 @@ class ServerManager: ServerInterface {
     }
 
     func placeOrder(items: [OrderItem]) -> Observable<Order> {
-        //print(Alamofire.request(APIRouter.placeOrder(order: items, userName: User.shared.name.value ?? "", fcmToken: ""), encoding: .JSON).debugDescription)
-//        let order = items.map {
-//            ["product_id": $0.product.id, "quantity": $0.amount]
-//        }
-//        let parameters: [String: Any] = ["user_name": User.shared.name.value ?? "", "fcm_token": "" ,"items": order]
-        //Alamofire.request(<#T##url: URLConvertible##URLConvertible#>, method: <#T##HTTPMethod#>, parameters: <#T##Parameters?#>, encoding: <#T##ParameterEncoding#>, headers: <#T##HTTPHeaders?#>)
-        //print(Alamofire.request("http://www.tritian.com/p3/api/orders", method: HTTPMethod.post, parameters: parameters, encoding: JSONEncoding.default, headers: nil).debugDescription)
         return requestJSON(router: APIRouter.placeOrder(order: items, userName: User.shared.name.value ?? "", fcmToken: "")).map { (urlResponse, jsonData) -> Order in
             guard let json = jsonData as? [String: Any] else {
                 throw RequestError.parsingError
